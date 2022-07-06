d3f2ef60dd8165fe49535d8ab5db0a878f06545a
Steven-Whitehead <Steven.Whitehead@ibm.com>
2022-07-06 13:56:45 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import os
+
+app = Flask(__name__)
+api = Api(app)
+port = int(os.environ.get('PORT') or 8080)
+
+class topic_tags(Resource):
+    def get(self):
+        return {'hello': 'world world'}
+
+api.add_resource(topic_tags, '/')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(port))
